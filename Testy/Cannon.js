fd99548b5945160b444fb8a7eb0214a73d71ed35
NiallFarrell <niall.farrell@ymail.com>
2013-06-13 04:21:59 +0100

    and more

--- Testy/Cannon.js
+++ Testy/Cannon.js
@@ -1,41 +1,66 @@
-function Cannon(mCanvas,mContext,mWorld,x,y)
+//var theBody;
+function Cannon(mCanvas,mContext,mWorld,mPos)
 {		
 	this.world = mWorld;
-	this.canvas = mCanvas;
-	this.context = mContext;
-	this.pos = new B2Vec2(x,y);
-	
+	//this.canvas = mCanvas;
+	//this.context = mContext;
 	this.fixDef = new B2FixtureDef();
+	this.bodyDef = new B2BodyDef();
+	this.pos = mPos;
+	this.MoveLeft = false;
+	this.MoveRight = false;
+	
 	this.fixDef.density = 1.0;	
 	this.fixDef.friction = 0.8;	
 	this.fixDef.restitution = 0.1;	
-	this.bodyDef = new B2BodyDef();
+	
+	this.DEGTORAD  = 0.0174532925199432957;
+	this.RADTODEG = 57.295779513082320876;
+	this.rot = new B2Vec2(1,1);
 	
 	// SETS DEFAULT DATA FOR THE OBJECT WHICH CAN BE RETRIEVED LATER
-	this.bodyDef.userData = "Cannon";
+	//this.bodyDef.userData = "Cannon";
 	
-	this.bodyDef.type = B2Body.b2_dynamicBody;
+	this.fixDef.shape = new B2PolygonShape();
+	this.fixDef.shape.SetAsBox(1.5/2 , 3/2);
 	
-	this.fixDef.shape = new b2PolygonShape();
-	this.fixDef.shape.m_radius = this.radius;
+	this.bodyDef.type = B2Body.b2_dynamicBody;   // b2_kinematicBody  // b2_dynamicBody // b2_staticBody
+	this.bodyDef.position.Set(this.pos.x,this.pos.y);
+	//bodyDef.position.Set(pos.x,pos.y);
 	
-	this.bodyDef.position.Set(this.pos.x, this.pos.y);
-	this.theBody = this.world.CreateBody(this.bodyDef);
+	this.theBody = world.CreateBody(this.bodyDef);
 	this.theBody.CreateFixture(this.fixDef);
-}
-
-function mys(mCanvas,mContext,mWorld,x,y)
-{
-}
-this.GameInput = function(keycode, TypeofEvent)
-{	
-	if(keycode == 37)//left arrow
-	{
-
-	}
 	
-	if(keycode == 39)//right arrow
-	{
+	this.pos = this.theBody.GetPosition();
+	this.rot.x = this.pos.x +2;
+	this.rot.y = this.pos.y +2;
+	this.theBody.GetWorldPoint(this.rot );
+	//theBody = world.CreateBody(this.bodyDef);
+	//theBody.CreateFixture(this.fixDef);
 
-	}
-}
+
+	this.Update = function()
+	{
+		if (this.MoveLeft == true)
+			{
+				//this.theBody.ApplyForce(new B2Vec2(40*-1,0),new B2Vec2(0,0));
+				//this.theBody.ApplyImpulse(new B2Vec2(0,-125),new B2Vec2(0,0));
+				//this.theBody.SetTransform(this.theBody.GetPosition(), -.45 * this.DEGTORAD );
+				//this.theBody.ApplyImpulse(new B2Vec2(-.001,0),this.theBody.GetWorldPoint( this.rot ) );
+				this.theBody.SetAngularVelocity(-1);
+				//this.theBody.SetTransform( this.theBody.GetPosition() , this.theBody.GetAngle() );
+				console.log("Left");
+			}
+		
+		else if (this.MoveRight == true)
+			{
+				//this.theBody.ApplyForce(new B2Vec2(40*1,0),new B2Vec2(0,0));
+				//this.theBody.ApplyImpulse(new B2Vec2(0,125),new B2Vec2(0,0));
+				//this.theBody.SetTransform(this.theBody.GetPosition(), .45 * this.DEGTORAD );
+				//this.theBody.ApplyImpulse(new B2Vec2(.001,0),this.theBody.GetWorldPoint( this.rot ) );    //this.theBody.GetWorldPoint( B2Vec2(1,1) ));
+				this.theBody.SetAngularVelocity(1);
+				console.log("Right");
+			}
+		else{this.theBody.SetAngularVelocity(0);}
+	};
+};
--- Testy/platform.js
+++ Testy/platform.js
@@ -4,13 +4,13 @@ var platformHeight = 0.5;
 if(drawPlatform == true)
 {
 	  //TOP
-	  var platformDef = new b2BodyDef;
-	  platformDef.type = b2Body.b2_staticBody;
+	  var platformDef = new B2BodyDef;
+	  platformDef.type = B2Body.b2_staticBody;
 	  platformDef.position.x = 18;
 	  platformDef.position.y = 20;
 	 
-	  var fixDefPlatform = new b2FixtureDef;
-	  fixDefPlatform.shape = new b2PolygonShape();
+	  var fixDefPlatform = new B2FixtureDef;
+	  fixDefPlatform.shape = new B2PolygonShape();
 	  fixDefPlatform.shape.SetAsBox(platformWidth , platformHeight);
 	  
 	  var platformF = world.CreateBody(platformDef);
